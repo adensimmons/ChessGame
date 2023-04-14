15bbfafb073a2de4cd632baaa39192fd15a6fc44
TyGuy275 <130717584+TyGuy275@users.noreply.github.com>
2023-04-14 08:17:11 -0400

    Add files via upload

---
+++ Chess/src/chess/Chess_Board.java
@@ -0,0 +1,22 @@
+package chess;
+
+public class Chess_Board {
+	public String[][] board = {{"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"}, 
+							   {"bx", "bx", "bx", "bx", "bx", "bx", "bx" ,"bx"}, 
+							   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
+							   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
+							   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
+							   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
+							   {"wx", "wx", "wx", "wx", "wx", "wx", "wx" ,"wx"}, 
+							   {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"}}; 
+	public void board() {
+		System.out.println(" ---------------------------------------");
+		for(int i = 0; i < board.length; i++) {
+			System.out.print("| ");
+			for(int j = 0; j < board.length; j++) {
+				System.out.print(board[i][j] + " | ");
+			}
+			System.out.println("\n ---------------------------------------");
+		}
+	}
+}
