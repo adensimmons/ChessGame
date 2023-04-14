990a5478af4d6c7da472b90594f40dc0bc01de8e
TyGuy275 <130717584+TyGuy275@users.noreply.github.com>
2023-04-14 08:18:33 -0400

    Update Chess_Board.java

--- Chess/src/chess/Chess_Board.java
+++ Chess/src/chess/Chess_Board.java
@@ -2,13 +2,13 @@ package chess;
 
 public class Chess_Board {
 	public String[][] board = {{"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"}, 
-							   {"bx", "bx", "bx", "bx", "bx", "bx", "bx" ,"bx"}, 
-							   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
-							   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
-							   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
-							   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
-							   {"wx", "wx", "wx", "wx", "wx", "wx", "wx" ,"wx"}, 
-							   {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"}}; 
+				   {"bx", "bx", "bx", "bx", "bx", "bx", "bx" ,"bx"}, 
+				   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
+				   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
+				   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
+				   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
+				   {"wx", "wx", "wx", "wx", "wx", "wx", "wx" ,"wx"}, 
+				   {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"}}; 
 	public void board() {
 		System.out.println(" ---------------------------------------");
 		for(int i = 0; i < board.length; i++) {
