adf736c83668a71d1f936304f2fbf58e53d06a4e
TyGuy275 <130717584+TyGuy275@users.noreply.github.com>
2023-04-14 08:59:00 -0400

    Update Chess_Board.java

--- Chess/src/chess/Chess_Board.java
+++ Chess/src/chess/Chess_Board.java
@@ -1,14 +1,26 @@
 package chess;
 
 public class Chess_Board {
-	public String[][] board = {{"br", "bn", "bb", "bq", "bk", "bb", "bn", "br"}, 
-				   {"bx", "bx", "bx", "bx", "bx", "bx", "bx" ,"bx"}, 
-				   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
-				   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
-				   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
-				   {"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, 
-				   {"wx", "wx", "wx", "wx", "wx", "wx", "wx" ,"wx"}, 
-				   {"wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"}}; 
+	public String[][] board = {{br, bn, bb, bq, bk, bb, bn, br}, 
+				   {bx, bx, bx, bx, bx, bx, bx, bx}, 
+				   {vo, vo, vo, vo, vo, vo, vo, vo}, 
+				   {vo, vo, vo, vo, vo, vo, vo, vo}, 
+				   {vo, vo, vo, vo, vo, vo, vo, vo}, 
+				   {vo, vo, vo, vo, vo, vo, vo, vo}, 
+				   {wx, wx, wx, wx, wx, wx, wx ,wx}, 
+				   {wr, wn, wb, wq, wk, wb, wn, wr}}; 
+	public String wq = "\u2655";
+        public String wr = "\u2656";
+        public String wb = "\u2657";
+        public String wn = "\u2658";
+        public String wx = "\u2659";
+        public String bk = "\u265A";
+        public String bq = "\u265B";
+        public String br = "\u265C";
+        public String bb = "\u265D";
+        public String bn = "\u265E";
+        public String bx = "\u265F";
+	public String vo = " ";
 	public void board() {
 		System.out.println(" ---------------------------------------");
 		for(int i = 0; i < board.length; i++) {
